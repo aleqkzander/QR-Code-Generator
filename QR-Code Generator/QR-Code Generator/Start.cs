ef6fbbe1edfdce23a39b8cbb67784944776bad9b
Alexander Lairich <alexanderlairich@web.de>
2023-12-28 13:22:14 +0100

    Initital project setup

---
+++ QR-Code Generator/QR-Code Generator/Start.cs
@@ -0,0 +1,15 @@
+namespace QR_Code_Generator
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
