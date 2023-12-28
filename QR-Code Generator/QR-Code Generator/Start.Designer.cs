44d7bfc7a58e5758c24e3d5a96bf66cfc0ce18cb
Alexander Lairich <alexanderlairich@web.de>
2023-12-28 14:11:18 +0100

    Creating the QR-Code and save it as png

--- QR-Code Generator/QR-Code Generator/Start.Designer.cs
+++ QR-Code Generator/QR-Code Generator/Start.Designer.cs
@@ -28,19 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            QRCodePicture = new PictureBox();
+            ContentBox = new TextBox();
+            GenerateBtn = new Button();
+            ExportBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)QRCodePicture).BeginInit();
             SuspendLayout();
+            // 
+            // QRCodePicture
+            // 
+            QRCodePicture.BorderStyle = BorderStyle.Fixed3D;
+            QRCodePicture.Location = new Point(12, 12);
+            QRCodePicture.Name = "QRCodePicture";
+            QRCodePicture.Size = new Size(300, 300);
+            QRCodePicture.SizeMode = PictureBoxSizeMode.Zoom;
+            QRCodePicture.TabIndex = 0;
+            QRCodePicture.TabStop = false;
+            // 
+            // ContentBox
+            // 
+            ContentBox.Location = new Point(318, 12);
+            ContentBox.Name = "ContentBox";
+            ContentBox.Size = new Size(300, 23);
+            ContentBox.TabIndex = 1;
+            // 
+            // GenerateBtn
+            // 
+            GenerateBtn.Location = new Point(318, 41);
+            GenerateBtn.Name = "GenerateBtn";
+            GenerateBtn.Size = new Size(300, 23);
+            GenerateBtn.TabIndex = 2;
+            GenerateBtn.Text = "Generate QR-Code";
+            GenerateBtn.UseVisualStyleBackColor = true;
+            GenerateBtn.Click += GenerateBtn_Click;
+            // 
+            // ExportBtn
+            // 
+            ExportBtn.Location = new Point(318, 70);
+            ExportBtn.Name = "ExportBtn";
+            ExportBtn.Size = new Size(300, 23);
+            ExportBtn.TabIndex = 3;
+            ExportBtn.Text = "Export QR-Code";
+            ExportBtn.UseVisualStyleBackColor = true;
+            ExportBtn.Click += ExportBtn_Click;
             // 
             // Start
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(658, 536);
+            ClientSize = new Size(630, 326);
+            Controls.Add(ExportBtn);
+            Controls.Add(GenerateBtn);
+            Controls.Add(ContentBox);
+            Controls.Add(QRCodePicture);
             Name = "Start";
             Text = "QR-Code Generator";
             Load += Start_Load;
+            ((System.ComponentModel.ISupportInitialize)QRCodePicture).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox QRCodePicture;
+        private TextBox ContentBox;
+        private Button GenerateBtn;
+        private Button ExportBtn;
     }
 }
--- QR-Code Generator/QR-Code Generator/Start.cs
+++ QR-Code Generator/QR-Code Generator/Start.cs
@@ -1,7 +1,11 @@
+using System.Drawing.Imaging;
+
 namespace QR_Code_Generator
 {
     public partial class Start : Form
     {
+        private QRCoder.QRCodeGenerator qrCodeGenerator;
+
         public Start()
         {
             InitializeComponent();
@@ -9,7 +13,24 @@ namespace QR_Code_Generator
 
         private void Start_Load(object sender, EventArgs e)
         {
+            qrCodeGenerator = new();
+        }
 
+        private void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            var qrData = qrCodeGenerator.CreateQrCode(ContentBox.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var qrCode = new QRCoder.QRCode(qrData);
+            QRCodePicture.Image = qrCode.GetGraphic(50);
+        }
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new();
+            saveFileDialog.Filter = "PNG files (*.png)|*.png";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                QRCodePicture.Image.Save(saveFileDialog.FileName + "." + ImageFormat.Png);
+            }
         }
     }
 }
